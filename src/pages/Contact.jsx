ffbdffad20d4e97557ef99ba85b85165003fc8ea
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-22 15:02:12 -0500

    added forms and fields to contact jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -39,11 +39,22 @@ export default function Contact() {
     // Validate the form before submission
     const newErrors = {};
     if (!formData.name) newErrors.name = 'This field is required';
+    if (!formData.email) newErrors.email = 'This field is required';
+    if (!errors.email && formData.email) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(formData.email)) {
+        newErrors.email = 'Invalid email address';
+      }
   }
 
+  setErrors(newErrors);
 
-
-
+  // If no errors, proceed with form submission
+  if (Object.keys(newErrors).length === 0) {
+    // Handle form submission
+    console.log(formData);
+    }
+  };
 
 
   return (
@@ -55,6 +66,43 @@ export default function Contact() {
           <li> GitHub: <a href="https://github.com/SherryK1103">GitHub Profile</a></li>
           <li> LinkedIn: <a href="https://www.linkedin.com/in/sherry-kneitz-560482186/">LinkedIn Profile</a></li>
         </ul>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            {errors.name && <p className="error">{errors.name}</p>}
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            {errors.email && <p className="error">{errors.email}</p>}
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Send</button>
+        </form>
       </section>
     </div>
   );
