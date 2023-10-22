1a6b26abfb6ce739fcbbb4058d35fcc3da08772d
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-22 14:26:45 -0500

    added linkedin to contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,6 +6,7 @@ export default function Contact() {
         <ul>
           <li> Email: <a href="mailto:sherrykneitz@gmail.com">sherrykneitz@gmail.com</a></li>
           <li> GitHub: <a href="https://github.com/SherryK1103">GitHub Profile</a></li>
+          <li> LinkedIn: <a href="https://www.linkedin.com/in/sherry-kneitz-560482186/">LinkedIn Profile</a></li>
         </ul>
       </section>
     </div>
