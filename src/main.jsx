a39e81af2fc1cbd9670de1715c65c54bb67145c8
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-23 07:04:17 -0500

    commented out 8-12 in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
+// import Error from './pages/Error';
+// import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Resume from './pages/Resume';
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
