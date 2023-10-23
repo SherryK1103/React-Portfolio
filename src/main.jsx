05f2541d40082638ba01a201bba8cb59a1a38808
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-23 07:14:32 -0500

    removed home from main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
-// import Error from './pages/Error';
-// import Contact from './pages/Contact';
-// import About from './pages/About';
-// import Portfolio from './pages/Portfolio';
-// import Resume from './pages/Resume';
+import Error from './pages/Error';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <About />,
       },
       {
         path: '/About',
