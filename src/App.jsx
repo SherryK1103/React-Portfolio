77849a8c65d1e6a2923e55629cb27aac6c7cb7ea
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-23 07:00:41 -0500

    edited app jsx and portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Header from './components/Header';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
@@ -9,6 +14,11 @@ function App() {
     <>
       <Header />
       <Nav />
+      <About />
+      <Contact />
+      <Error />
+      <Portfolio />
+      <Resume />
       <main className="mx-3">
         <Outlet />
       </main>
