c4f2a479c02119564e862ea126044ca67296dec5
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-28 09:08:04 -0500

    added deployed links and commented out unneeded code in portfolio jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,18 +6,23 @@ import weatherDash from '../assets/images/WEATHER-DASH-SS.png';
 import workDay from '../assets/images/workday-scheduler-5.png';
 
 
-function ProjectCard({ name, description, link, imageSrc }) {
+function ProjectCard({ name, description, link, deployedLink, imageSrc }) {
   return (
     <div className="card">
       <h4>{name}</h4>
       <p>{description}</p>
-      <a href={link}>
-        <img src={imageSrc} alt={`screenshot of ${name}`} className={`${name}-ss`} />
+      <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+        View Deployed App
       </a>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        View GitHub Repository
+      </a>
+      <img src={imageSrc} alt={`screenshot of ${name}`} className={`${name}-ss`} />
       <p id={`${name.toLowerCase().replace(/ /g, '-')}-title`}>{name}</p>
     </div>
   );
 }
+
 
 function Projects() {
   const projectData = [
@@ -25,6 +30,7 @@ function Projects() {
       name: 'Green Thumb Guide',
       description: 'A website where you can search a plant database on how to best care for your indoor plants.',
       link: 'https://github.com/SherryK1103/Green-Thumb-Guide',
+      deployedLink: 'https://green-thumb-guide-dbdcfacbedc6.herokuapp.com/',
       imageSrc: { greenThumb },
     },
     {
@@ -37,24 +43,28 @@ function Projects() {
       name: 'Note Taker',
       description: 'A platform that enables users to write and save notes. This platform aims to facilitate users in organizing their thoughts and tracking tasks that need completion.',
       link: 'https://github.com/SherryK1103/Note-Taker-08',
+      deployedLink: 'https://intense-chamber-09330-07a5b76c6b1e.herokuapp.com/',
       imageSrc: { notePhoto },
     },
     {
       name: '50 States Flash Facts',
       description: 'A website where you can easily learn some basic and quick facts about each of the 50 United States of America in a flashcard-like format.',
       link: 'https://github.com/SherryK1103/Group-Challenge-01-50-States',
+      deployedLink: 'https://sherryk1103.github.io/Group-Challenge-01-50-States/',
       imageSrc: { fiftyStates },
     },
     {
       name: 'Weather Dashboard',
       description: 'An application where the user can view the current day weather as well as a 5 day forecast for any city that typed into the search bar, and view all previously searched cities.',
       link: 'https://github.com/SherryK1103/Weather-Dashboard-06',
+      deployedLink: 'https://sherryk1103.github.io/Weather-Dashboard-06/',
       imageSrc: { weatherDash },
     },
     {
       name: 'Workday Scheduler',
       description: 'A web application where the user can schedule their day down to the hour.',
       link: 'https://github.com/SherryK1103/Workday-Scheduler-5',
+      deployedLink: 'https://sherryk1103.github.io/Workday-Scheduler-5/',
       imageSrc: { workDay },
     },
   ];
