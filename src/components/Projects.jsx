2016d839ed7cdce436f5041d0fac38840cf755c5
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-28 08:54:50 -0500

    added export portfolio function in portfolio jsx and edited projects in projects jsx file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,10 @@
 import greenThumb from '../assets/images/GREEN-THUMB-SS.png';
-// import all other photos up here
 import mvcPhoto from '../assets/images/MVC-SS.png';
+import notePhoto from '../assets/images/NOTE-TAKER-SC.png';
+import fiftyStates from '../assets/images/50-STATES-SCREENSHOT.png';
+import weatherDash from '../assets/images/WEATHER-DASH-SS.png';
+import workDay from '../assets/images/workday-scheduler-5.png';
+
 
 function ProjectCard({ name, description, link, imageSrc }) {
   return (
@@ -23,41 +27,35 @@ function Projects() {
       link: 'https://github.com/SherryK1103/Green-Thumb-Guide',
       imageSrc: { greenThumb },
     },
-    // Add data for other projects here
-
     {
       name: 'MVC Tech Blog',
-      description: 'Write about MVC Tech Blog here.',
+      description: 'A CMS-style blog site with the purpose of publishing articles, blog posts, and sharing thoughts and opinions.',
       link: 'https://github.com/SherryK1103/MVC-Tech-Blog',
       imageSrc: { mvcPhoto },
     },
-
     {
       name: 'Note Taker',
-      description: 'A website where you can search a plant database on how to best care for your indoor plants.',
-      link: 'https://github.com/SherryK1103/Green-Thumb-Guide',
-      imageSrc: { greenThumb },
+      description: 'A platform that enables users to write and save notes. This platform aims to facilitate users in organizing their thoughts and tracking tasks that need completion.',
+      link: 'https://github.com/SherryK1103/Note-Taker-08',
+      imageSrc: { notePhoto },
     },
-
     {
       name: '50 States Flash Facts',
-      description: 'A website where you can search a plant database on how to best care for your indoor plants.',
-      link: 'https://github.com/SherryK1103/Green-Thumb-Guide',
-      imageSrc: { greenThumb },
+      description: 'A website where you can easily learn some basic and quick facts about each of the 50 United States of America in a flashcard-like format.',
+      link: 'https://github.com/SherryK1103/Group-Challenge-01-50-States',
+      imageSrc: { fiftyStates },
     },
-
     {
       name: 'Weather Dashboard',
-      description: 'A website where you can search a plant database on how to best care for your indoor plants.',
-      link: 'https://github.com/SherryK1103/Green-Thumb-Guide',
-      imageSrc: { greenThumb },
+      description: 'An application where the user can view the current day weather as well as a 5 day forecast for any city that typed into the search bar, and view all previously searched cities.',
+      link: 'https://github.com/SherryK1103/Weather-Dashboard-06',
+      imageSrc: { weatherDash },
     },
-
     {
       name: 'Workday Scheduler',
-      description: 'A website where you can search a plant database on how to best care for your indoor plants.',
-      link: 'https://github.com/SherryK1103/Green-Thumb-Guide',
-      imageSrc: { greenThumb },
+      description: 'A web application where the user can schedule their day down to the hour.',
+      link: 'https://github.com/SherryK1103/Workday-Scheduler-5',
+      imageSrc: { workDay },
     },
   ];
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,124 +1,128 @@
-import Projects from '../components/Projects'
-// leave outer divs, leave h1, sections id my work and main content
+import Projects from '../components/Projects';
 
 export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-
-      <section id="my-work" className="main-content">
-        {/* Project One ********************************************************/}
-        <div>
-          <h4>Green Thumb Guide</h4>
-          <p id="green-title">Green Thumb Guide</p>
-        </div>
-
-        {/* Project Two ********************************************************/}
-        <div className="card">
-          <h4>MVC Tech Blog</h4>
-          <p>
-            The SEO HTML Refactor assignment was my first project at the
-            NorthWestern University Coding Bootcamp. We were given a webpage
-            with code and were charged with consolidating redundant code,
-            changing it from non-semantic elements to semantic elements to
-            improve SEO as well as comply with AAA standards. This assignment
-            displayed the skills I had learned in HTML and CSS.
-          </p>
-          <p id="mvc-title">MVc Tech Blog</p>
-        </div>
-
-        {/* Project Three ****************************************************/}
-        <div className="card">
-          <h4>Note Taker</h4>
-          <p>
-            The SEO HTML Refactor assignment was my first project at the
-            NorthWestern University Coding Bootcamp. We were given a webpage
-            with code and were charged with consolidating redundant code,
-            changing it from non-semantic elements to semantic elements to
-            improve SEO as well as comply with AAA standards. This assignment
-            displayed the skills I had learned in HTML and CSS.
-          </p>
-           {/* Add link to deployed site */}
-          <a href="https://github.com/SherryK1103/Note-Taker-08">
-            <img
-              src="../assets/images/NOTE-TAKER-SC.png"
-              alt="screenshot of note taker"
-              className="note-photo"
-            />
-          </a>
-          <p id="note-title">Note Taker</p>
-        </div>
-
-        {/* Project Four ********************************************************/}
-        <div className="card">
-          <h4>50 States Flash Facts</h4>
-          <p>
-            The SEO HTML Refactor assignment was my first project at the
-            NorthWestern University Coding Bootcamp. We were given a webpage
-            with code and were charged with consolidating redundant code,
-            changing it from non-semantic elements to semantic elements to
-            improve SEO as well as comply with AAA standards. This assignment
-            displayed the skills I had learned in HTML and CSS.
-          </p>
-           {/* Add link to deployed site */}
-          <a href="https://github.com/SherryK1103/Group-Challenge-01-50-States">
-            <img
-              src="../assets/images/50-STATES-SCREENSHOT.png"
-              alt="screenshot of 50 states project"
-              className="50-photo"
-            />
-          </a>
-          <p id="50-title">50 States Flash Facts</p>
-        </div>
-
-        {/* Project Five *******************************************************/}
-        <div className="card">
-          <h4>Weather Dashboard</h4>
-          <p>
-            The SEO HTML Refactor assignment was my first project at the
-            NorthWestern University Coding Bootcamp. We were given a webpage
-            with code and were charged with consolidating redundant code,
-            changing it from non-semantic elements to semantic elements to
-            improve SEO as well as comply with AAA standards. This assignment
-            displayed the skills I had learned in HTML and CSS.
-          </p>
-           {/* Add link to deployed site */}
-          <a href="https://github.com/SherryK1103/Weather-Dashboard-06">
-            <img
-              src="../assets/images/WEATHER-DASH-SS.png"
-              alt="screenshot of weather dashboard"
-              className="weather-photo"
-            />
-          </a>
-          <p id="weather-title">Weather Dashboard</p>
-        </div>
-
-        {/* Project Six **********************************************************/}
-        <div className="card">
-          <h4>Workday Scheduler</h4>
-          <p>
-            The SEO HTML Refactor assignment was my first project at the
-            NorthWestern University Coding Bootcamp. We were given a webpage
-            with code and were charged with consolidating redundant code,
-            changing it from non-semantic elements to semantic elements to
-            improve SEO as well as comply with AAA standards. This assignment
-            displayed the skills I had learned in HTML and CSS.
-          </p>
-           {/* Add link to deployed site */}
-          <a href="https://sherryk1103.github.io/SEO_HTML-Refactor/">
-            <img
-              src="../assets/images/workday-scheduler-5.png"
-              alt="screenshot of workday scheduler"
-              className="first-webpage-photo"
-            />
-          </a>
-          <p id="project_one_title">SEO HTML Refactor</p>
-        </div>
-
-      
-        </div>
-
-      </section>
+      <Projects /> {/* Include the Projects component */}
     </div>
   );
 }
+
+
+
+// export default function Portfolio() {
+//   return (
+//     <div>
+//       <h1>Portfolio</h1>
+
+//       <section id="my-work" className="main-content">
+//         {/* Project One *************************************/}
+//         <div>
+//           <h4>Green Thumb Guide</h4>
+//           <p id="green-title">Green Thumb Guide</p>
+//         </div>
+
+//         {/* Project Two ***********************************************/}
+//         <div className="card">
+//           <h4>MVC Tech Blog</h4>
+//           <p>
+//             The SEO HTML Refactor assignment was my first project at the
+//             NorthWestern University Coding Bootcamp. We were given a webpage
+//             with code and were charged with consolidating redundant code,
+//             changing it from non-semantic elements to semantic elements to
+//             improve SEO as well as comply with AAA standards. This assignment
+//             displayed the skills I had learned in HTML and CSS.
+//           </p>
+//           <p id="mvc-title">MVC Tech Blog</p>
+//         </div>
+
+//         {/* Project Three ***************************************/}
+//         <div className="card">
+//           <h4>Note Taker</h4>
+//           <p>
+//             The SEO HTML Refactor assignment was my first project at the
+//             NorthWestern University Coding Bootcamp. We were given a webpage
+//             with code and were charged with consolidating redundant code,
+//             changing it from non-semantic elements to semantic elements to
+//             improve SEO as well as comply with AAA standards. This assignment
+//             displayed the skills I had learned in HTML and CSS.
+//           </p>
+//           <a href="https://github.com/SherryK1103/Note-Taker-08">
+//             <img
+//               src="../assets/images/NOTE-TAKER-SC.png"
+//               alt="screenshot of note taker"
+//               className="note-photo"
+//             />
+//           </a>
+//           <p id="note-title">Note Taker</p>
+//         </div>
+
+//         {/* Project Four **************************************************/}
+//         <div className="card">
+//           <h4>50 States Flash Facts</h4>
+//           <p>
+//             The SEO HTML Refactor assignment was my first project at the
+//             NorthWestern University Coding Bootcamp. We were given a webpage
+//             with code and were charged with consolidating redundant code,
+//             changing it from non-semantic elements to semantic elements to
+//             improve SEO as well as comply with AAA standards. This assignment
+//             displayed the skills I had learned in HTML and CSS.
+//           </p>
+//           <a href="https://github.com/SherryK1103/Group-Challenge-01-50-States">
+//             <img
+//               src="../assets/images/50-STATES-SCREENSHOT.png"
+//               alt="screenshot of 50 states project"
+//               className="50-photo"
+//             />
+//           </a>
+//           <p id="50-title">50 States Flash Facts</p>
+//         </div>
+
+//         {/* Project Five ******************************************************/}
+//         <div className="card">
+//           <h4>Weather Dashboard</h4>
+//           <p>
+//             The SEO HTML Refactor assignment was my first project at the
+//             NorthWestern University Coding Bootcamp. We were given a webpage
+//             with code and were charged with consolidating redundant code,
+//             changing it from non-semantic elements to semantic elements to
+//             improve SEO as well as comply with AAA standards. This assignment
+//             displayed the skills I had learned in HTML and CSS.
+//           </p>
+//           {/* Add link to deployed site */}
+//           <a href="https://github.com/SherryK1103/Weather-Dashboard-06">
+//             <img
+//               src="../assets/images/WEATHER-DASH-SS.png"
+//               alt="screenshot of weather dashboard"
+//               className="weather-photo"
+//             />
+//           </a>
+//           <p id="weather-title">Weather Dashboard</p>
+//         </div>
+
+//         {/* Project Six *******************************************************/}
+//         <div className="card">
+//           <h4>Workday Scheduler</h4>
+//           <p>
+//             The SEO HTML Refactor assignment was my first project at the
+//             NorthWestern University Coding Bootcamp. We were given a webpage
+//             with code and were charged with consolidating redundant code,
+//             changing it from non-semantic elements to semantic elements to
+//             improve SEO as well as comply with AAA standards. This assignment
+//             displayed the skills I had learned in HTML and CSS.
+//           </p>
+//           {/* Add link to deployed site */}
+//           <a href="https://sherryk1103.github.io/SEO_HTML-Refactor/">
+//             <img
+//               src="../assets/images/workday-scheduler-5.png"
+//               alt="screenshot of workday scheduler"
+//               className="first-webpage-photo"
+//             />
+//           </a>
+//           <p id="project_one_title">SEO HTML Refactor</p>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// }
