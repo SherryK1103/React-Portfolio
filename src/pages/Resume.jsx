9795d8aaaaa136c9dddcb4196395194507cf6a1b
SherryK1103 <129017117+SherryK1103@users.noreply.github.com>
2023-10-28 12:06:28 -0500

    removed resume and added download resume link plus blurb about myself

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -3,77 +3,11 @@ import '../styles/Resume.css';
 const Resume = () => {
   return (
     <div>
-      <h1>Sherry Kneitz</h1>
-      <p className="email">Email: sherrykneitz@gmail.com</p>
-      
-      <div className="job-entry">
-        <h2>TekSynap Corporation, Reston, VA (Remote)</h2>
-        <p>Configuration Analyst - November 2021 to Present</p>
-        <ul>
-          <li>Process floorplans using AutoCad to create deliverable packages</li>
-          <li>Modify existing files, images, reports, documentation, etc. as required</li>
-          <li>Analyzing, tracking, and preparation of documentation throughout lifecycle</li>
-          <li>Creating different types of files (PDF, JPEGs, PNGs, etc.,)</li>
-          <li>Create and maintain multiple reports throughout lifecycle for delivery to the customer</li>
-          <li>Review all deliverables for accuracy</li>
-          <li>Follow provided SOP</li>
-          <li>Maintain documentation in proper file structure, naming convention, etc.</li>
-        </ul>
-      </div>
-
-      <div className="job-entry">
-        <h2>Brown and Joseph LLC, Itasca, IL</h2>
-        <p>Supervisor, Skip Tracing and Pre-Litigation Operations - November 2020 to Present</p>
-        <p>Skip Tracer and Litigation Coordinator - June 2018 to December 2019</p>
-        <ul>
-          <li>Effectively complete skip tracing a high volume of accounts on a daily basis</li>
-          <li>Supervise comprehensive case investigations for clients: evaluate past efforts, manage data and conduct research, evaluate and project potential for litigation, and provide official recommendation</li>
-          <li>Manage regular communications with legal, compliance, financial, government, and risk/insurance institutions</li>
-          <li>Prepare comprehensive analyses on D&B reports to support financial stability assessments</li>
-          <li>Apply analytic techniques to complex information sets in order to make logical and supported recommendations</li>
-          <li>Complete litigation reviews at a rate 150% above industry standard</li>
-          <li>Move 90% of prospects to production; highest success rate in the company</li>
-        </ul>
-      </div>
-
-      <div className="job-entry">
-        <h2>Myers Service, Inc. dba MSI Detective Services, Chicago, IL</h2>
-        <p>Director of Special Investigations - April 2017 to May 2018</p>
-        <p>Senior Investigator - September 2013 to March 2017</p>
-        <p>Special Investigator - March 2013 to September 2013</p>
-        <ul>
-          <li>Analyzed data to develop qualified leads that resulted in cases solved, contributing to a 60% increase in solved cases 2015-2018</li>
-          <li>Successfully managed and directed skip tracing and locate investigations daily</li>
-          <li>Managed all matters of process service: assigned process to servers, drafted and perfected affidavits for process service and complex locate investigations</li>
-          <li>Directed and managed surveillance and undercover investigations (Workers Compensation; Personal Injury; Medical Malpractice; Criminal, Civil)</li>
-          <li>Ensured compliance with privacy laws: i.e. GLBA (Gramm Bliley Leach Act), the DPPA (Drivers Privacy Protection Act), and FCRA (Fair Credit Reporting Act)</li>
-        </ul>
-      </div>
-
-      <h2>Qualifications</h2>
-      <ul>
-        <li>Expert in proprietary/investigative databases i.e. TLO, Tracers, IRBSearch (Accurint, LexisNexis)</li>
-        <li>Proficient in Microsoft Suite, including Microsoft Office</li>
-        <li>Experienced with Microsoft Word, Microsoft Excel, and Microsoft Teams</li>
-        <li>Experienced in data entry and customer service</li>
-        <li>Proficient in Salesforce</li>
-        <li>Strong organizational skills</li>
-        <li>Experienced in working cross-functionally and in a team environment while managing multiple priorities</li>
-        <li>Proficient with varied levels of internal/external client communications</li>
-      </ul>
-
-      <h2>Volunteer Work</h2>
-      <ul>
-        <li>Street Reach - Organization serving meals and homeless outreach (Thompson Center)</li>
-        <li>Cradles to Crayons Nonprofit managing donations for childrens items</li>
-        <li>Veterans organizations and Senior Assisted Living Centers Choirs and singing performances</li>
-      </ul>
-
-      <h2>Education</h2>
-      <ul>
-      <li>Northwestern University - EdEx Fullstack Web Development Bootcamp </li>
-      <li>Elgin Community College</li>
-      </ul>
+      <h1>Sherry Kneitz Resume</h1>
+      <p>I am a dedicated and experienced professional with a strong background in operations, investigations, and analysis. With a proven track record of effective case management and data analysis, I have successfully supervised teams, managed complex investigations, and maintained a high level of accuracy and compliance in my work. My experience includes process management, documentation preparation, and a deep understanding of proprietary investigative databases. I am a proficient user of various software tools and have a strong aptitude for cross-functional teamwork. In addition to my professional work, I'm passionate about volunteer work, including helping the homeless, supporting children's nonprofits, and participating in community activities. I'm also committed to continuous learning and recently completed the Northwestern University EdEx Fullstack Web Development Bootcamp.</p>
+      <a href="https://docs.google.com/document/d/1Czj3OX7-QHnIuQOPrjbMYuQsBAXlDMniP_cVCiXP29c/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+        Download Resume
+      </a>
     </div>
   );
 };
